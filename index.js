de4cd35d7f871bba5c992d0230495e4e73038e08
salamanderctesiphon <swbrookshire@gmail.com>
2023-03-03 15:15:17 -0600

    examples

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ console.log(user.name);
 delete user.name;
 
 console.log(user.name); */
+//write a function isEmpty(obj) which returns true if the object has no properties
+
+let schedule = {};
+
+function isEmpty(schedule) {
+    for (let key in schedule) {
+       return false;
+    } 
+    return true;
+}
+
+console.log( isEmpty(schedule) ); // true
+
+schedule["8:30"] = "get up";
+
+console.log( isEmpty(schedule) ); 
