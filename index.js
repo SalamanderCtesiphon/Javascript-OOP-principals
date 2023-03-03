b6ba16e1c7ff5f3444a0f12acf5d86628b37dbe7
salamanderctesiphon <swbrookshire@gmail.com>
2023-03-03 16:48:34 -0600

    working ahead with objects

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+function Person(name) {
+   this.name = name;
+   this.introduceSelf = function() {
+   console.log(`Hi I'm ${this.name}.`); 
+   }
+};
+
+const salva = new Person("Salva");
+salva.name;
+salva.introduceSelf();
+
+const frankie = new Person("Frankie");
+frankie.name;
+frankie.introduceSelf();
 
 
