e76bd976fc1649ac7e20bf1b0f606a01549f205b
salamanderctesiphon <swbrookshire@gmail.com>
2023-03-03 16:21:03 -0600

    working throught the objects lesson

--- index.js
+++ index.js
@@ -1,27 +1,2 @@
-/* let user = {};
 
-user.name = 'John';
-user.surname = "Smith";
-user.name = 'Pete';
 
-console.log(user.name);
-
-delete user.name;
-
-console.log(user.name); */
-//write a function isEmpty(obj) which returns true if the object has no properties
-
-let schedule = {};
-
-function isEmpty(schedule) {
-    for (let key in schedule) {
-       return false;
-    } 
-    return true;
-}
-
-console.log( isEmpty(schedule) ); // true
-
-schedule["8:30"] = "get up";
-
-console.log( isEmpty(schedule) ); 
